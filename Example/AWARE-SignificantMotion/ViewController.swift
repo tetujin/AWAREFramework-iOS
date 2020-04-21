430ab7cceb61116d421735a1c337ec2791f2ce90
Yuuki Nishiyama <tetujin@ht.sfc.keio.ac.jp>
2020-04-22 08:57:03 +0900

    Version 1

---
+++ Example/AWARE-SignificantMotion/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AWARE-SignificantMotion
+//
+//  Created by Yuuki Nishiyama on 2020/04/21.
+//  Copyright © 2020 tetujin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
